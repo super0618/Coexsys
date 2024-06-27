25e60e91e52651a0f3f2f4d7124669137aba608f
superdev <newestStar1990@gmail.com>
2024-06-27 05:29:23 -0400

    feat: added license

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import { registerLicense } from "@syncfusion/ej2-base";
+
+registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY)
 </script>
 
 <template>
