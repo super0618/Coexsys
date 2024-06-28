98d7dbe19d61bcd7d11c6e4f9820ab9b9dc35fa5
devsenior <seniordev1220@gmail.com>
2024-06-28 19:24:20 -0400

    feat: update baseline and criticalpath, add tasktimeline

--- src/components/main/criticalpath.vue
+++ src/components/main/criticalpath.vue
@@ -4,42 +4,40 @@ import { GanttComponent, Toolbar, Edit, Selection, CriticalPath } from "@syncfus
 provide('gantt', [Edit,Toolbar, Selection, CriticalPath])
 import {projectNewData} from '@/data/tabledata'
 
-const height = "600px";
-const projectStartDate = new Date('03/24/2024');
-const projectEndDate = new Date('07/06/2024');
-const taskFields = {
-    id: 'TaskID',
-    name: 'TaskName',
-    startDate: 'StartDate',
-    endDate: 'EndDate',
-    duration: 'Duration',
-    progress: 'Progress',
-    dependency: 'Predecessor',
-    child: 'subtasks'
-};
-const editSettings = {
-    allowAdding: true,
-    allowEditing: true,
-    allowDeleting: true,
-    allowTaskbarEditing: true,
-    showDeleteConfirmDialog: true
-};
-const toolbar = ['Add', 'Edit','Delete','CriticalPath'];
-const labelSettings = {
-    leftLabel: 'TaskName'
-};
-const columns = [
-    { field: 'TaskID', width: 80 },
-    { field: 'TaskName',headerText: 'Name', width: 250 },
-    { field: 'StartDate' },
-    { field: 'EndDate' },
-    { field: 'Duration' },
-    { field: 'Predecessor' },
-    { field: 'Progress' },
-]
-const splitterSettings = {
-    columnIndex: 4
-};
+const 
+    height = "600px",
+    taskFields = {
+        id: 'TaskID',
+        name: 'TaskName',
+        startDate: 'StartDate',
+        endDate: 'EndDate',
+        duration: 'Duration',
+        progress: 'Progress',
+        dependency: 'Predecessor',
+        child: 'subtasks'
+    },
+    toolbar = ['Add', 'Edit','Delete','CriticalPath'],
+    splitterSettings = { columnIndex: 4 },
+    columns = [
+        { field: 'TaskID', width: 80 },
+        { field: 'TaskName',headerText: 'Name', width: 250 },
+        { field: 'StartDate' },
+        { field: 'EndDate' },
+        { field: 'Duration' },
+        { field: 'Predecessor' },
+        { field: 'Progress' },
+    ],
+    editSettings = {
+        allowAdding: true,
+        allowEditing: true,
+        allowDeleting: true,
+        allowTaskbarEditing: true,
+        showDeleteConfirmDialog: true
+    },
+    labelSettings = { leftLabel: 'TaskName' },
+    projectStartDate = new Date('03/24/2024'),
+    projectEndDate = new Date('07/06/2024')
+
 </script>
 <template>
     <GanttComponent
